b433f0556a8c4377f4f9caa02cd05de388c1ebf9
Renan Ricoldi Fróis Pedro <renan.ricoldi@uel.br>
2019-07-26 00:53:42 -0300

    Logo adicionada e inicio dos arquivos

---
+++ src/pages/main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, Image, StatusBar, Dimensions} from 'react-native'
+
+const Main = () => {
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default Main
